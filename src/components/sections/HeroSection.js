3d09cab6f3aed5fb33b43ad53bba7238379f9bf1
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-08-09 17:25:39 +0200

    (feat) add responsiveness to most of the home page

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -4,16 +4,16 @@ import heroImage from "@/assets/images/hero-image.jpg";
 
 export default function HeroSection() {
   return (
-    <div className="flex w-screen justify-center bg-primaryGreen py-4">
-      <div className="flex h-96 w-full max-w-5xl flex-wrap">
-        <div className="flex w-[60%] flex-col justify-between py-4">
-          <div className="font-markazi flex flex-col">
+    <div className="flex w-screen justify-center bg-primaryGreen px-4 py-4 lg:px-0">
+      <div className="flex w-full max-w-5xl flex-col lg:h-96 lg:flex-row">
+        <div className="flex flex-col gap-8 py-4 lg:w-[60%] lg:justify-between lg:gap-0">
+          <div className="flex flex-col font-markazi">
             <h1 className="text-7xl font-medium text-primaryYellow">
               Little Lemon
             </h1>
             <h3 className="text-4xl text-white">Chicago</h3>
           </div>
-          <p className="w-96 text-white">
+          <p className="text-white lg:w-96">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
             minima fugiat omnis nesciunt voluptate rerum veritatis sequi quidem
             temporibus nobis, laboriosam, inventore dicta, reiciendis officiis
@@ -21,13 +21,13 @@ export default function HeroSection() {
           </p>
           <YellowButton text={"Reserve a Table"} url={"/reserve-table"} />
         </div>
-        <div className="relative w-[40%]">
+        <div className="lg:relative lg:w-[40%]">
           <Image
             src={heroImage}
             width={410}
             height={500}
             alt="Photo of a chief holding a plate full of dishes"
-            className="absolute h-[450px] w-full rounded-2xl object-cover"
+            className="h-[350px] w-full rounded-2xl object-cover lg:absolute lg:h-[450px]"
           />
         </div>
       </div>
--- src/components/sections/Highlights.js
+++ src/components/sections/Highlights.js
@@ -18,13 +18,13 @@ export default function Highlights() {
   return (
     <div className="flex w-screen flex-col items-center bg-white py-12 lg:flex-row lg:justify-center">
       <div className="w-full lg:max-w-5xl">
-        <div className="flex h-56 flex-wrap items-center justify-between pb-8">
+        <div className="flex h-56 flex-wrap items-center justify-center pb-8 lg:justify-between lg:pb-0">
           <h3 className="font-markazi text-5xl lg:text-7xl">
             This weeks specials!
           </h3>
           <YellowButton text={"Online Menu"} url={"/menu"} />
         </div>
-        <div className="flex flex-wrap gap-8">{specials}</div>
+        <div className="flex w-full flex-wrap gap-8">{specials}</div>
       </div>
     </div>
   );
