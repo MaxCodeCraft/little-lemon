982bb3640bccb32383d4789d8d34e6a0a81d7d9e
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-08-09 16:04:30 +0200

    (feat) add testimonials section

---
+++ src/components/cards/TestimonialCard.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function TestimonialCard(props) {
+  const stars = [];
+  for (let i = 0; i < props.rating; i++) {
+    stars.push(
+      <FontAwesomeIcon
+        icon={faStar}
+        key={i}
+        size="lg"
+        className="w-4 text-primaryYellow"
+      />,
+    );
+  }
+  for (let i = props.rating; i < 5; i++) {
+    stars.push(
+      <FontAwesomeIcon
+        icon={faStarRegular}
+        key={i}
+        size="lg"
+        className="w-4 text-primaryYellow"
+      />,
+    );
+  }
+  return (
+    <div className="flex flex-col gap-4 rounded-2xl bg-white p-4 shadow-lg">
+      <div className="flex">{stars}</div>
+      <div className="flex gap-4">
+        <Image
+          src={props.photo}
+          width={112}
+          height={112}
+          className="h-28 w-28 rounded-full"
+        />
+        <p className="w-80">{`"${props.review.slice(0, 150)}"`}</p>
+      </div>
+      <p className="cursor-pointer font-bold underline">{props.username}</p>
+    </div>
+  );
+}
--- src/components/sections/Testimonials.js
+++ src/components/sections/Testimonials.js
@@ -1,3 +1,29 @@
+import TestimonialCard from "../cards/TestimonialCard";
+import userRatings from "@/data/ratings";
+
+const testimonials = userRatings.map((rating) => {
+  return (
+    <TestimonialCard
+      key={rating.id}
+      username={rating.username}
+      rating={rating.rating}
+      review={rating.review}
+      photo={rating.photo}
+    />
+  );
+});
+
 export default function Testimonials() {
-  return;
+  return (
+    <div className="flex w-screen justify-center bg-primaryGreen py-10">
+      <div className="flex w-full max-w-5xl flex-col items-center gap-8 py-8">
+        <h3 className="font-markazi text-5xl text-primaryYellow lg:text-7xl">
+          Testimonials
+        </h3>
+        <div className="flex w-full gap-5 overflow-x-auto py-8">
+          {testimonials}
+        </div>
+      </div>
+    </div>
+  );
 }
