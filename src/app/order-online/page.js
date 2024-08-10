46dc65a84be3b5ffcb6ab74006519fd1f2b60941
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-08-10 07:07:24 +0200

    (fix) change path to imgs

--- src/app/order-online/page.js
+++ src/app/order-online/page.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import lemonIcon from "@/assets/images/lemon-icon.png";
 
 export default function OrderOnline() {
   return (
     <div className="flex h-[700px] w-screen items-center justify-center bg-primaryGreen">
       <div className="flex h-full w-full max-w-5xl py-32">
         <div className="flex w-1/2 items-center justify-center">
-          <Image src={lemonIcon} width={200} height={500} />
+          <Image src={"/static/img/lemon-icon.png"} width={200} height={500} />
         </div>
         <div className="flex w-1/2 flex-col justify-center gap-32">
           <h1 className="font-markazi text-7xl font-medium text-primaryYellow">
